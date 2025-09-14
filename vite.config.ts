c23673b3ebd972fcf8a9353aeb793adde9136649
dgusgus <dgusgus1@gmail.com>
2025-09-14 09:38:51 -0400

    creamos al estructura de carpetas y corregimos tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+
+      // 🔹 Core app structure
+      '@api': fileURLToPath(new URL('./src/api', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@config': fileURLToPath(new URL('./src/config', import.meta.url)),
+      '@features': fileURLToPath(new URL('./src/modules', import.meta.url)),
+      '@providers': fileURLToPath(new URL('./src/providers', import.meta.url)),
+      '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url))
+    }
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
 })
